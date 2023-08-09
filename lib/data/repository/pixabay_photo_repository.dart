f1860579e146a15e155b3f9622531c461398a1db
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-08-10 00:44:14 +0900

    [이미지 검색앱] pixabay_photo_repository 작성

---
+++ lib/data/repository/pixabay_photo_repository.dart
@@ -0,0 +1,18 @@
+import 'package:image_search_wednesday2/data/data_source/pixabay_api.dart';
+import 'package:image_search_wednesday2/data/mapper/photo_mapper.dart';
+import 'package:image_search_wednesday2/domain/model/photo.dart';
+import 'package:image_search_wednesday2/domain/repository/photo_repository.dart';
+
+class PixabayPhotoRepositoryImpl implements PhotoRepository {
+  final _api = PixabayApi();
+
+  @override
+  Future<List<Photo>> getPhotos(String query) async {
+    final resultDto = await _api.getImages(query);
+
+    if (resultDto.hits == null) {
+      return [];
+    }
+    return resultDto.hits!.map((e) => e.toPhoto()).toList();
+  }
+}
