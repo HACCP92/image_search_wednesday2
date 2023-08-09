14444850aa3c0b4ecfe8c893441a04676f956a70
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-08-10 00:43:14 +0900

    [이미지 검색앱] photo_mapper 작성

---
+++ lib/data/mapper/photo_mapper.dart
@@ -0,0 +1,16 @@
+import 'package:image_search_wednesday2/data/dto/pixabay_result_dto.dart';
+import 'package:image_search_wednesday2/domain/model/photo.dart';
+
+extension ToPhoto on Hits {
+  Photo toPhoto() {
+    return Photo(
+      id: id ?? 0,
+      views: views ?? 0,
+      likes: likes ?? 0,
+      webformatURL: webformatURL ?? '',
+      tags: tags ?? '',
+      user: user ?? '',
+      userImageURL: userImageURL ?? '',
+    );
+  }
+}
